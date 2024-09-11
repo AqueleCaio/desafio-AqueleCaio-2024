4bba5a1b15faa2c7c95c279f93cf62054c2c387d
AqueleCaio <106452341+AqueleCaio@users.noreply.github.com>
2024-09-11 16:57:00 -0300

    inicio

---
+++ src/recintos-zoo.js
@@ -0,0 +1,22 @@
+/*
+Um animal se sente confortável se está num bioma adequado e com espaço suficiente para cada indivíduo
+Animais carnívoros devem habitar somente com a própria espécie
+Animais já presentes no recinto devem continuar confortáveis com a inclusão do(s) novo(s)
+Hipopótamo(s) só tolera(m) outras espécies estando num recinto com savana e rio
+Um macaco não se sente confortável sem outro animal no recinto, seja da mesma ou outra espécie
+Quando há mais de uma espécie no mesmo recinto, é preciso considerar 1 espaço extra ocupado
+Não é possível separar os lotes de animais nem trocar os animais que já existem de recinto (eles são muito apegados!). Por exemplo, se chegar um lote de 12 macacos, não é possível colocar 6 em 2 recintos.
+*/
+
+class RecintosZoo {
+
+    // Verifica se o animal pode entrar naquele recinto, seguinda
+    // as regras de negócio
+    analisaRecintos(animal, quantidade){
+        
+
+    }
+
+}
+
+export { RecintosZoo as RecintosZoo };
